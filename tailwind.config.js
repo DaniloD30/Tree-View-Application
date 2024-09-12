fd6983f62b8591bab45fee08cd6697483c553d81
Danilo Dias <danilolive30@hotmail.com>
2024-09-12 01:15:08 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter"],
+      },
+      colors: {
+        blue: {
+          100: "#E3EAEF",
+          500: "#2188FF",
+          950: "#17192D",
+        },
+        gray: {
+          600: "#77818C",
+        },
+      },
+      screens: {
+        xs: "375px",
+        sm: "480px",
+        md: "760px",
+        lg: "960px",
+        default: "1200px",
+      },
+    },
+  },
+  plugins: [],
+};
